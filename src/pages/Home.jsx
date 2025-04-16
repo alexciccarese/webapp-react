b71ecdedfa9901438d22f87aceef6966d74ec1db
alexciccarese <alex.ciccarese@gmail.com>
2025-04-16 15:49:13 +0200

    fix: ensure movies are displayed correctly in Home component

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,18 +5,28 @@ export default function Home() {
 
   useEffect(() => {
     fetch('http://localhost:3004/api/movies')
-    .then(res => res.json())
-    .then(data => {
-      console.log(data)
-      setMovies(data)
-    })
-
+      .then(res => res.json())
+      .then(data => {
+        console.log(data)
+        setMovies(data)
+      })
+      .catch((err) => console.error('Error fetching movies:', err))
   }, [])
 
   return (
 
     <>
       <h1>Home page</h1>
+
+      {movies.length > 0 ? (
+        <ul>
+          {movies.map((movie) => (
+            <li key={movie.id}>{movie.title}</li>
+          ))}
+        </ul>
+      ) : (
+        <p>No movies available.</p>
+      )}
     </>
   )
 }
