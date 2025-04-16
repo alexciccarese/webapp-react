3b6d5e10d99da812544e8e9b353fbb67780cf5a9
alexciccarese <alex.ciccarese@gmail.com>
2025-04-16 14:52:01 +0200

    feat: fetch and display movies in Home component

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,4 +1,17 @@
+import { useState, useEffect } from "react"
+
 export default function Home() {
+  const [movies, setMovies] = useState([])
+
+  useEffect(() => {
+    fetch('http://localhost:3004/api/movies')
+    .then(res => res.json())
+    .then(data => {
+      console.log(data)
+      setMovies(data)
+    })
+
+  }, [])
 
   return (
 
