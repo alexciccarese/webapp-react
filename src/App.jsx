0242dd460d9b940faade1f35e70aec54a581aa47
alexciccarese <alex.ciccarese@gmail.com>
2025-04-16 12:15:18 +0200

    feat: add routing with DefaultLayout and bootstrap dependencies
    - Added react-router-dom for routing functionality.
    - Created DefaultLayout component to structure the app layout with header and footer.
    - Updated App component to use BrowserRouter and Routes for navigation.
    - Added bootstrap and bootstrap-icons for styling.
    - Created placeholder Home page and movieObj page files.

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,18 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom"
+import DefaultLayout from "./layouts/DefaultLayout"
+
 export default function App() {
-
-  return(
-
+  return (
     <>
-    <h1>Hello world!</h1>
+      <BrowserRouter>
+        <Routes>
+          <Route Component={DefaultLayout}>
+
+            <Route path="/" element={<h1>Hello world!</h1>} />
+
+          </Route>
+        </Routes>
+      </BrowserRouter>
     </>
   )
 }
