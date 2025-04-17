f8b71eb6c3791cacb6ae3346608e4abe53dae041
alexciccarese <alex.ciccarese@gmail.com>
2025-04-17 17:39:36 +0200

    refactor: enhance movie card layout and update image source handling in Home component

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -14,30 +14,41 @@ export default function Home() {
       .catch((err) => console.error('Error fetching movies:', err))
   }, [])
 
+
+  const imageMap = {
+    'The Matrix': 'matrix.jpg',
+    'Inception': 'inception.jpg',
+    'Interstellar': 'interstellar.jpg',
+    'The good father': 'the_goodfather.jpg',
+    'titanic': 'titanic.jpg',
+  }
+
   return (
 
     <>
-      <h1>Home page</h1>
 
       <section className="movies">
         <div className="container">
-          <div className="row">
+      <h1>Home page</h1>
+          <div className="row row-cols-1 row-cols-md-2 row-cols-lg-3 g-4">
 
             {movies.length > 0 ? (
 
-              
-                movies.map((movie) => (
-                  <div key={movie.id} className="col">
-                    <div className="card">
-                      <Link to={`/${movie.id}`}>
-                      <img src={movie.image} alt={movie.title} />
-                      <h3>{movie.title}</h3>
-                      <p>{movie.release_year}</p>
-                      </Link>
-                    </div>
+
+              movies.map((movie) => (
+                <div key={movie.id} className="col">
+                  <div className="card">
+                    <Link to={`/${movie.id}`}>
+                      <div className="card-body">
+                        <img src={`http://localhost:3004/cover_image/${movie.title.toLowerCase()}.jpg`} alt={movie.title} />
+                        <h3>{movie.title}</h3>
+                        <p>{movie.release_year}</p>
+                      </div>
+                    </Link>
                   </div>
-                ))
-              
+                </div>
+              ))
+
             ) : (
               <p>No movies available.</p>
             )}
