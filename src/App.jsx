d98f584363402a0edcaef4603105a51ac6e1d77d
alexciccarese <alex.ciccarese@gmail.com>
2025-04-22 17:44:21 +0200

    style: add NotFound component for handling 404 errors and update routing in App component

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom"
 import DefaultLayout from "./layouts/DefaultLayout"
 import Home from "./pages/Home"
 import movieObj from "./pages/movieObj"
+import NotFound from "./pages/NotFound"
 
 export default function App() {
   return (
@@ -12,6 +13,8 @@ export default function App() {
 
             <Route path="/" Component={Home} />
             <Route path="/:id" Component={movieObj} />
+
+            <Route path="/*" Component={NotFound} />
 
           </Route>
         </Routes>
