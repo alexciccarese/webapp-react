294a1863e8b61b78c7cae29db1e0e1671fbe7c34
alexciccarese <alex.ciccarese@gmail.com>
2025-04-22 16:04:57 +0200

    style: enhance movieObj component layout with improved structure and review display

--- src/pages/movieObj.jsx
+++ src/pages/movieObj.jsx
@@ -6,7 +6,7 @@ export default function movieObj() {
   const [movie, setMovie] = useState(null)
 
   useEffect(() => {
-    fetch(`http://localhost:3004/api/movies/${id}`)
+    fetch(`http://localhost:3004/api/movies/` + id)
       .then(res => res.json())
       .then(data => {
         console.log(data)
@@ -16,17 +16,43 @@ export default function movieObj() {
   }, [id])
 
   return (
+
     <>
-      {movie ? (
-        <>
-          <h1>{movie.title}</h1>
-          <img src={`http://localhost:3004/cover_image/${movie.image}`} alt={movie.title} />
-          <p>{movie.release_year}</p>
-          <p>{movie.description}</p>
-        </>
+
+      <div className="p-5 mb-4 bg-light rounded-3">
+        <div className="container py-5">
+          <h1 className="display-5 fw-bold">{movie?.title}</h1>
+          <p className="col-md-8 fs-4">
+            {movie?.abstract}
+          </p>
+
+        </div>
+      </div>
+
+
+      {movie?.reviews && movie.reviews.length > 0 ? (
+        <div>
+          <h2>Reviews</h2>
+          
+          {movie.reviews.map(review => (
+            <div className="card" key={review.id}>
+
+              <div className="card-header d-flex justify-content-between align-items-center">
+                <h3>{review?.text}</h3>
+                <div className="vote">{review?.vote}</div>
+              </div>
+
+              <div className="card-body">
+                {review?.text}
+              </div>
+            </div>
+          ))}
+        </div>
       ) : (
-        <p>Loading...</p>
+        <p>No reviews yet!</p>
       )}
+
+
     </>
   )
 }
