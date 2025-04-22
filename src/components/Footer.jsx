498cb753c7ae256e75d02bddc2a9b86763c8ec36
alexciccarese <alex.ciccarese@gmail.com>
2025-04-22 12:00:50 +0200

    style: update Footer component structure and styling for improved layout

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,7 +2,33 @@ export default function Footer() {
 
   return (
     <>
-      <h1>Footer</h1>
+      <footer className="py-4 bg-light ">
+        <div className="container">
+          <div className="row">
+
+            <div className="col-12 col-md-4">
+              <div className="logo">
+                LOGO
+              </div>
+              <p>
+                <i className="bi bi-copyright">All rights reserved</i>
+              </p>
+            </div>
+
+            <div className="col-12 col-md-4">
+              <h3>Links</h3>
+              <ul className="list-unstyled">
+
+                <li><a href="#" className="nav-link">Lorem</a></li>
+                <li><a href="#" className="nav-link">Lorem</a></li>
+                <li><a href="#" className="nav-link">Lorem</a></li>
+                <li><a href="#" className="nav-link">Lorem</a></li>
+              </ul>
+            </div>
+
+          </div>
+        </div>
+      </footer>
     </>
   )
 }
